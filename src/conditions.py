180c506dae287ff03315f7d0c14baa637fe0eab7
Paul Krüger <p.krueger@muehlhan.com>
2022-05-11 17:25:21 +0200

    'condition-checks'

--- src/conditions.py
+++ src/conditions.py
@@ -1,13 +1,26 @@
 # Basischeck
-def check_shipyard ():
-    return bool (len(me.shipyards) == 1)
+def check_shipyard (board):
+    me = board.current_player
+    return bool (len(me.shipyards) == 0)
 
-# Bester Schiffbauer
-def best_port
+# Besten Port Builder finden
+def best_port_builder (board):
+    me = board.current_player
+    for ship in me.ships:
+        global port_builder
+        port_builder = ship.ID (max (me.ships))
+    return port_builder
+        
+# Port bauen
+def build_port():
+    if check_port(board):
+        port_builder.next_action = ShipAction.CONVERT
 
-# Rollenzuweisung
-def check-role ():
-    return bool()
-
-# Entfernung zur Basis
-def 
+# Zur Basis zurückkehren    
+def return_to_shipyard():
+    me = board.current_player
+    for ship in me.ships:
+        if ship_states[ship.id] == "DEPOSIT":
+            direction = getDirTo(ship.position, me.shipyards[0].position, size)
+            if direction:
+                ship.next_action = direction
