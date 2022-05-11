857c35b1405807e704f5853f9274a95d4a1dad15
Janosch Hoefer <30659014+JaNoNi@users.noreply.github.com>
2022-05-11 20:29:51 +0200

    added attacker role

--- src/attackers.py
+++ src/attackers.py
@@ -1,4 +1,3 @@
-# Imports helper functions
 import random
 
 import numpy as np
@@ -7,9 +6,7 @@ from kaggle_environments.envs.halite.helpers import *
 from numpy import linalg as LA
 
 
-# Returns best direction to move from one position (fromPos) to another (toPos)
-# Example: If I'm at pos 0 and want to get to pos 55, which direction should I choose?
-def getDirTo(fromPos, toPos, size):
+def getDirTo(fromPos: tuple[int, int], toPos: tuple[int, int], size):
     fromX, fromY = divmod(fromPos[0], size), divmod(fromPos[1], size)
     toX, toY = divmod(toPos[0], size), divmod(toPos[1], size)
     if fromY < toY:
@@ -83,60 +80,29 @@ def attack(
     return direction
 
 
-# Directions a ship can move
-directions = [ShipAction.NORTH, ShipAction.EAST, ShipAction.SOUTH, ShipAction.WEST]
+def main():
+    NUM_AGENTS = 4
+    BOARD_SIZE = 7
+    TURNS = 6
 
-# Will keep track of whether a ship is collecting halite or carrying cargo to a shipyard
-ship_states = {}
+    env = make(
+        "halite",
+        configuration={"randomSeed": 1, "episodeSteps": TURNS, "size": BOARD_SIZE},
+        debug=True,
+    )
+    _ = env.reset(num_agents=NUM_AGENTS)
 
-# Returns the commands we send to our ships and shipyards
-def agent(obs, config):
-    size = config.size
-    board = Board(obs, config)
-    me = board.current_player
+    # As example take the first frame of the game
+    sample_obs = env.state[0].observation
+    board = Board(sample_obs, env.configuration)
 
     feature = world_feature(board)
 
-    # If there are no ships, use first shipyard to spawn a ship.
-    if len(me.ships) == 0 and len(me.shipyards) > 0:
-        me.shipyards[0].next_action = ShipyardAction.SPAWN
+    me = board.current_player
+    attacker_id = [ship.id for ship in me.ships][0]
+    shipPos = [ship.position for iid, ship in board.ships.items() if iid == attacker_id][0]
+    attack(shipPos, board, feature)
 
-    # If there are no shipyards, convert first ship into shipyard.
-    if len(me.shipyards) == 0 and len(me.ships) > 0:
-        me.ships[0].next_action = ShipAction.CONVERT
 
-    for ship in me.ships:
-        if ship.next_action == None:
-
-            ### Part 1: Set the ship's state
-            # if ship.halite < 100:
-            ship_states[ship.id] = "ATTACK"
-            # if ship.halite < 200:  # If cargo is too low, collect halite
-            # ship_states[ship.id] = "COLLECT"
-            # if ship.halite > 500:  # If cargo gets very big, deposit halite
-            # ship_states[ship.id] = "DEPOSIT"
-
-            ### Part 2: Use the ship's state to select an action
-            if ship_states[ship.id] == "COLLECT":
-                # If halite at current location running low,
-                # move to the adjacent square containing the most halite
-                if ship.cell.halite < 100:
-                    neighbors = [
-                        ship.cell.north.halite,
-                        ship.cell.east.halite,
-                        ship.cell.south.halite,
-                        ship.cell.west.halite,
-                    ]
-                    best = max(range(len(neighbors)), key=neighbors.__getitem__)
-                    ship.next_action = directions[best]
-            if ship_states[ship.id] == "DEPOSIT":
-                # Move towards shipyard to deposit cargo
-                direction = getDirTo(ship.position, me.shipyards[0].position, size)
-                if direction:
-                    ship.next_action = direction
-            if ship_states[ship.id] == "ATTACK":
-                direction = attack(ship.position, board, feature)
-                if direction:
-                    ship.next_action = direction
-
-    return me.next_actions
+if __name__ == "__main__":
+    main()
